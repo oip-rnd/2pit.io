cd4ad2c7898d084123f1b6fe782137c5f3ba6c16
bruno <bruno@macbook-pro-de-bruno.home>
2018-12-22 00:39:31 +0100

    fix content undefined

--- vendor/PpitFlow/src/Controller/FunnelController.php
+++ vendor/PpitFlow/src/Controller/FunnelController.php
@@ -302,7 +302,6 @@ class FunnelController extends AbstractActionController
 			'place_identifier' => $place_identifier,
 			'funnel' => $funnel,
 			'step' => $step,
-			'content' => $content,
 			'commitments' => $commitments,
 			'actionStatus' => $actionStatus,
 		));
