dfcd039afc24b389efa2a9fef4eab7c35f3eebcd
bruno <bruno@macbook-pro-de-bruno.home>
2019-06-08 23:40:09 +0200

    Multi fqdn to a same instance

--- vendor/PpitCore/src/Controller/HomeController.php
+++ vendor/PpitCore/src/Controller/HomeController.php
@@ -14,7 +14,8 @@ class HomeController extends AbstractActionController
 		$instance_caption = $this->params()->fromRoute('instance_caption', null);
 		if ($instance_caption) Context::$instance = Instance::get($instance_caption, 'caption');
 		if (!$context->isAuthenticated()) {
-			if (array_key_exists($context->getInstance()->fqdn, $context->getConfig()['fqdnToInstance'])) return $this->redirect()->toRoute($context->getConfig()['fqdnToInstance'][$context->getInstance()->fqdn]['defaultRoute']);
+			$host = $this->request->getUri()->getHost();
+			if (array_key_exists($host, $context->getConfig()['fqdnToInstance'])) return $this->redirect()->toRoute($context->getConfig()['fqdnToInstance'][$host]['defaultRoute']);
 			if ($context->getInstance()->home_page) return $this->redirect()->toRoute($context->getInstance()->home_page);
 			else return $this->redirect()->toRoute('user/login', array('instance_caption' => ($instance_caption) ? $instance_caption : $context->getInstance()->caption));
 		}
