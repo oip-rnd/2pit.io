03cb2ea380327539f33879aac2f83751bf050100
bruno <bruno@MacBook-Pro-de-Bruno.local>
2019-07-01 13:04:07 +0200

    Accept non-email as identifier in password init panel

--- vendor/PpitCore/src/Controller/UserController.php
+++ vendor/PpitCore/src/Controller/UserController.php
@@ -970,8 +970,8 @@ class UserController extends AbstractActionController
 					return $this->getResponse();
 				}
 				$token = $context->getSecurityAgent()->requestPasswordInit($user, false, $this->url());
-				$userContact = UserContact::transGet($user->user_id, 'user_id');
-				$vcard = Vcard::transGet($userContact->vcard_id);
+				$userContact = UserContact::getTable()->transGet($user->user_id, 'user_id');
+				$vcard = Vcard::getTable()->transGet($userContact->vcard_id);
 
 				// Send the OTP by email
 				$email_body = $context->localize($context->getConfig('user/messages/lost_password/text'));
